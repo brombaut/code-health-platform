d5d912f90295bb0efd6455d29422746a0a8be2e1
Ben Rombaut <ben.rombaut@huawei.com>
2022-03-03 15:28:40 -0500

    Generate callgraph for java jar files and add to structure json file

--- demos/callgraphs/structure_and_dependency_merger/structure_and_dependency_merger.py
+++ demos/callgraphs/structure_and_dependency_merger/structure_and_dependency_merger.py
@@ -4,33 +4,37 @@ import logging
 import os
 from itertools import groupby
 
+DEPENDENCIES_KEY = "$dependencies"
+CHILDREN_KEY = "children"
+NAME_KEY = "name_no_extension"
+
 
 def read_json_file(file_path):
     with open(file_path, 'r') as f:
         return json.load(f)
 
 
+def write_json_file(data, file_path):
+    with open(file_path, 'w') as f:
+        json.dump(data, f)
+
+
 def read_txt_file(file_path):
     with open(file_path, 'r') as f:
         return f.readlines()
 
-
-def remove_file_extention(name):
-    return os.path.splitext(name)[0]
-
-
 class Structure:
     def __init__(self, structure):
-        self.structure = structure
+        self.root = structure
 
     def structure_as_lines(self):
         lines = []
-        self.__rec_transform_structure_to_lines(self.structure, [], lines)
+        self.__rec_transform_structure_to_lines(self.root, [], lines)
         return lines
 
     def __rec_transform_structure_to_lines(self, root, components_so_far, result):
         # Add root name to component_so_far
-        components_so_far.append(remove_file_extention(root['name']))
+        components_so_far.append(root[NAME_KEY])
         if len(root['children']) == 0:
             # Base case - leaf node, so add the path here to the result
             result.append(components_so_far)
@@ -40,8 +44,18 @@ class Structure:
                 # Copy the list of components so far because
                 self.__rec_transform_structure_to_lines(c, components_so_far.copy(), result)
 
-    def merge_with_structure(self, other):
-        pass
+    def merge_in_dependency_data(self, other):
+        self.__rec_merge(self.root, other.root)
+
+    def __rec_merge(self, node1, node2):
+        for child2 in node2[CHILDREN_KEY]:
+            if child2[NAME_KEY] == DEPENDENCIES_KEY:
+                node1[DEPENDENCIES_KEY] = child2[CHILDREN_KEY]
+            else:
+                # Find matched child node in node1 children
+                child1 = next((item for item in node1[CHILDREN_KEY] if item[NAME_KEY] == child2[NAME_KEY]), None)
+                if child1:
+                    self.__rec_merge(child1, child2)
 
 
 class ParserBuilder:
@@ -58,97 +72,105 @@ class ParserBuilder:
 
 
 class JavaCallgraphParser:
+
+    class LineComponent:
+        def __init__(self, dependency_line, structure_lines):
+            self.dependency_line = dependency_line
+            self.structure_lines = structure_lines
+            self.dependency_type, self.from_components, self.to_components = self.__line_components()
+
+        def __line_components(self):
+            dependency_type = self.dependency_line[0]
+            rest_of_line = self.dependency_line[2:]
+            rest_of_line = rest_of_line.rstrip("\n")
+            from_str = rest_of_line.split(" ")[0]
+            to_str = rest_of_line.split(" ")[1]
+            from_components_dependency = [self.remove_anonymous_inner_class(s) for s in from_str.split(".")]
+            from_components_structure = self.__add_prefix_path_match_from_strucutre(from_components_dependency)
+            to_components_dependency = [self.remove_anonymous_inner_class(s) for s in to_str.split(".")]
+            to_components_structure = self.__add_prefix_path_match_from_strucutre(to_components_dependency)
+            return dependency_type, from_components_structure, to_components_structure
+
+        def __add_prefix_path_match_from_strucutre(self, dependency_component):
+            result = dependency_component
+            for structure_line in self.structure_lines:
+                matched_path = self.__match_structure_path_to_dependency_path(structure_line, result)
+                if matched_path:
+                    result = matched_path + result
+            return result
+
+        @staticmethod
+        def remove_anonymous_inner_class(s):
+            return s.split("$")[0]
+
+        @staticmethod
+        def __match_structure_path_to_dependency_path(structure_path, dependency_path):
+            s_idx = len(structure_path) - 1
+            d_idx = len(dependency_path) - 1
+            # Walk the whole dependency structure path in reverse
+            while d_idx >= 0:
+                # If the dependency path doesn't match the structure path, then they don't match, so return nothing
+                if dependency_path[d_idx] != structure_path[s_idx]:
+                    return None
+                # Current dependency path component and current structure path component match.
+                # Move onto the next one
+                s_idx -= 1
+                d_idx -= 1
+            # We have matched to whole dependency path to the structure path.
+            # So the remaining structure path is the prefix, which we return
+            return structure_path[:s_idx + 1]
+
     def __init__(self, file_path, structure):
-        self.lines = read_txt_file(file_path)
-        self.structure = structure
-        self.path_prefix = self.__determine_path_prefix()
-        self.dependency_structure = dict()
+        lines = read_txt_file(file_path)
+        structure_lines = structure.structure_as_lines()
+        self.line_components = [self.LineComponent(line, structure_lines) for line in lines]
+        self.dependency_structure = {
+            NAME_KEY: "root",
+            CHILDREN_KEY: []
+        }
 
     def parse(self):
         self.__build_dependency_structure()
 
-    def __determine_path_prefix(self):
-        structure_lines = self.structure.structure_as_lines()
-        matches = list()
-        for dependency_line in self.lines:
-            dependency_type, from_components, to_components = self.__line_components(dependency_line)
-            # Only do this for the class relationships just to speed things up
-            if dependency_type != "C":
-                continue
-            for structure_line in structure_lines:
-                matched_path = self.__match_structure_path_to_dependency_path(structure_line, from_components)
-                if matched_path:
-                    matches.append(matched_path)
-
-        def all_equal(iterable):
-            g = groupby(iterable)
-            return next(g, True) and not next(g, False)
-        if all_equal(matches):
-            return matches[0]
-        raise Exception("Not able to extract path prefix")
-
-    @staticmethod
-    def __match_structure_path_to_dependency_path(structure_path, dependency_path):
-        s_idx = len(structure_path) - 1
-        d_idx = len(dependency_path) - 1
-        # Walk the whole dependency structure path in reverse
-        while d_idx >= 0:
-            # If the dependency path doesn't match the structure path, then they don't match, so return nothing
-            if dependency_path[d_idx] != structure_path[s_idx]:
-                return None
-            # Current dependency path component and current structure path component match.
-            # Move onto the next one
-            s_idx -= 1
-            d_idx -= 1
-        # We have matched to whole dependency path to the structure path.
-        # So the remaining structure path is the prefix, which we return
-        return structure_path[:s_idx + 1]
-
     def __build_dependency_structure(self):
-        for line in self.lines:
-            self.__parse_line(line)
+        for line_component in self.line_components:
+            self.__parse_line(line_component)
 
-    def __line_components(self, line):
-        def remove_anonymous_inner_class(s):
-            return s.split("$")[0]
-        dependency_type = line[0]
-        rest_of_line = line[2:]
-        rest_of_line = rest_of_line.rstrip("\n")
-        from_str = rest_of_line.split(" ")[0]
-        to_str = rest_of_line.split(" ")[1]
-        from_components = [remove_anonymous_inner_class(s) for s in from_str.split(".")]
-        if hasattr(self, "path_prefix"):
-            from_components = self.path_prefix + from_components
-        to_components = [remove_anonymous_inner_class(s) for s in to_str.split(".")]
-        if hasattr(self, "path_prefix"):
-            to_components = self.path_prefix + to_components
-        return dependency_type, from_components, to_components
-
-    def __parse_line(self, line):
-        dependency_type, from_components, to_components = self.__line_components(line)
-        if dependency_type == "C":
-            self.__parse_class_dependency(from_components, to_components)
-        elif dependency_type == "M":
+    def __parse_line(self, line_component):
+        if line_component.dependency_type == "C":
+            self.__parse_class_dependency(line_component.from_components, line_component.to_components)
+        elif line_component.dependency_type == "M":
             pass
         else:
-            logging.error(f"Unknown dependency type: {dependency_type}")
+            logging.error(f"Unknown dependency type: {line_component.dependency_type}")
 
     def __parse_class_dependency(self, from_components, to_components):
-        self.__add_from_component_to_dependency_tree(self.dependency_structure, from_components, to_components)
+        # Ignore first element of from_component , which is root
+        self.__add_from_component_to_dependency_tree(self.dependency_structure, from_components[1:], to_components)
 
     def __add_from_component_to_dependency_tree(self, dependencies_tree, from_components, to_components):
         if len(from_components) == 0:
             # Base case - add dependency to leaf node
-            if "$dependencies" not in dependencies_tree:
-                dependencies_tree["$dependencies"] = dict()
-            self.__add_to_components_to_from(dependencies_tree["$dependencies"], to_components)
+            dependencies_child = next((item for item in dependencies_tree[CHILDREN_KEY] if item[NAME_KEY] == DEPENDENCIES_KEY), None)
+            if not dependencies_child:
+                dependencies_child = {
+                    NAME_KEY: DEPENDENCIES_KEY,
+                    CHILDREN_KEY: []
+                }
+                dependencies_tree[CHILDREN_KEY].append(dependencies_child)
+            self.__add_to_components_to_from(dependencies_child, to_components)
         else:
             curr_from_component = from_components[0]
             remaining_from_components = from_components[1:]
-            if curr_from_component not in dependencies_tree:
-                dependencies_tree[curr_from_component] = dict()
+            next_component_node = next((item for item in dependencies_tree[CHILDREN_KEY] if item[NAME_KEY] == curr_from_component), None)
+            if not next_component_node:
+                next_component_node = {
+                    NAME_KEY: curr_from_component,
+                    CHILDREN_KEY: [],
+                }
+                dependencies_tree[CHILDREN_KEY].append(next_component_node)
             self.__add_from_component_to_dependency_tree(
-                dependencies_tree[curr_from_component],
+                next_component_node,
                 remaining_from_components,
                 to_components
             )
@@ -160,12 +182,14 @@ class JavaCallgraphParser:
         else:
             curr_to_component = to_components[0]
             remaining_to_components = to_components[1:]
-            # Remove anything after '$' character - threading stuff
-            if "$" in curr_to_component:
-                curr_to_component = curr_to_component.split("$")[0]
-            if curr_to_component not in dependencies_tree:
-                dependencies_tree[curr_to_component] = dict()
-            self.__add_to_components_to_from(dependencies_tree[curr_to_component], remaining_to_components)
+            next_component_node = next((item for item in dependencies_tree[CHILDREN_KEY] if item[NAME_KEY] == curr_to_component), None)
+            if not next_component_node:
+                next_component_node = {
+                    NAME_KEY: curr_to_component,
+                    CHILDREN_KEY: [],
+                }
+                dependencies_tree[CHILDREN_KEY].append(next_component_node)
+            self.__add_to_components_to_from(next_component_node, remaining_to_components)
 
 
 def main(args):
@@ -173,7 +197,9 @@ def main(args):
     dependency_parser = ParserBuilder(args.callgraph_generator, args.callgraph_file, structure).parser()
     dependency_parser.parse()
     dependency_structure = Structure(dependency_parser.dependency_structure)
-    merged = structure.merge_with_structure(dependency_structure)
+    # write_json_file(dependency_parser.dependency_structure, "./dependency_data.json")
+    structure.merge_in_dependency_data(dependency_structure)
+    write_json_file(structure.root, "./aug_structure_data.json")
 
 
 if __name__ == "__main__":
